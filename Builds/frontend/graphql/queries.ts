68537678f4e7ad6bd4cd153a4b0d261bbd7c25a7
roshray <roshanroy759@gmail.com>
2022-05-27 11:11:51 +0530

    Feed component

--- Builds/frontend/graphql/queries.ts
+++ Builds/frontend/graphql/queries.ts
@@ -1,5 +1,22 @@
 import { gql } from "@apollo/client";
 
+export const GET_ALL_POSTS = gql`
+
+    query MyQuery {
+        getPostList {
+            body
+            created_at
+            id
+            image
+            title
+            subreddit_id
+            username
+        }
+    }
+
+`
+
+
 export const GET_SUBREDDIT_BY_TOPIC = gql`
     query MyQuery($topic: String!) {
         getSubredditListByTopic(topic: $topic) {
